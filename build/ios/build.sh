bb30a706caae71abebb8fa0b135fea6aa6d3d0fb
root <chaowang1@alauda.io>
2026-05-31 11:21:38 +0800

    release v0.2.1

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="CodeSwitchX"
 BUNDLE_ID="com.codeswitch-x.app"
-VERSION="0.2.0"
-BUILD_NUMBER="0.2.0"
+VERSION="0.2.1"
+BUILD_NUMBER="0.2.1"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
